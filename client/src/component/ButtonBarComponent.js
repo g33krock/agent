bf515e279e999a0fc41b03eb838940fcf478ba2c
g33krock <56360847+g33krock@users.noreply.github.com>
2021-12-22 08:48:20 -0700

    Add files via upload

---
+++ client/src/component/ButtonBarComponent.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { agentService } from "../services/AgentService";
+
+export class ButtonBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      agent: this.props.agent,
+    };
+  }
+
+  async componentDidMount() {
+    const ID = this.props.Id;
+    const agentObject = { agentID: ID };
+    const agent = await agentService.one(agentObject);
+    this.setState({ agent });
+    console.log(this.props.agent);
+    console.log(ID);
+  }
+
+  render() {
+    return (
+      <div>
+        <Container fluid style={{ flexDirection: "row" }}>
+          <Row
+            className="justify-content-md-center"
+            style={{ marginBottom: "25px", marginTop: "25px" }}
+          >
+              <Col sm={2}></Col>
+            <Col sm={3}>
+              <Button
+                style={{
+                  // backgroundColor: `${this.props.agent.secondaryColor}`,
+                  backgroundColor: `black`,
+                  width: "100%",
+                  border: "none",
+                  boxShadow: "2px 2px rgba(0, 0, 0, 0.5)",
+                  color: `${this.props.agent.textColor}`,
+                  marginBottom: "5px"
+                }}
+                href="https://compoundinterest.com/main-page"
+              >
+                SECURE COMPOUND
+                <br />
+                INTEREST VIDEO SERIES
+              </Button>{" "}
+            </Col>
+            <Col sm={2}></Col>
+            <Col sm={3}>
+              <Button
+                style={{
+                  // backgroundColor: `${this.props.agent.secondaryColor}`,
+                  backgroundColor: `black`,
+                  color: `${this.props.agent.textColor}`,
+                  width: "100%",
+                  border: "none",
+                  boxShadow: "2px 2px rgba(0, 0, 0, 0.5)",
+                  marginBottom: "5px"
+                }}
+                href="https://www.mympi.com/mpi/mpi-calculator"
+              >
+                MPI® RETIREMENT
+                <br />
+                CALCULATOR
+              </Button>{" "}
+            </Col>
+            <Col sm={2}></Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
---
+++ client/src/component/MainComponent.js
@@ -0,0 +1,81 @@
+import React, { useState } from "react";
+import { Col, Row } from "react-bootstrap";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import { agentService } from "../services/AgentService";
+import { About } from "./AboutComponent";
+// import { Bottom } from "./BottomComponent";
+import { Calculator } from "./CalculatorComponent";
+import { Calendar } from "./CalendarComponent";
+import { FAQ } from "./FAQComponent";
+import { Footer } from "./FooterComponent";
+import { Header2 } from "./Header2Component";
+
+import { Home } from "./HomeComponent";
+import { VideoSeries } from "./VideoSeriesComponent";
+
+export function Main() {
+  const agentId = 4;
+  const agentObject = { agentID: agentId };
+  const [agent] = useState(() => {
+    const initialAgent = agentService.one(agentObject);
+    return initialAgent;
+  });
+
+  console.log({ agent });
+  console.log({ agentObject });
+  return (
+    <div style={{minHeight: "100vh"}}>
+      {/* <video autoPlay loop muted id="video">
+        <source
+          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/videos/waterVideo.mp4"
+          type="video/mp4"
+        />
+      </video> */}
+ 
+              <img 
+              src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/images/SDRPBackdrop.png"
+              alt="DS"
+              className="backdrop"/>
+
+      <Header2 agent={agent[0]} Id={agentId} style={{ marginLeft: "0px" }} />
+      <Calendar agent={agent[0]} Id={agentId}></Calendar>
+      <Row style={{ marginRight: "0px", marginLeft: "0px", width: "100%", minHeight: "80vh" }}>
+        <Col style={{ paddingRight: "0px", paddingLeft: "0px" }}>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={<Home agent={agent[0]} Id={agentId} />}
+              />
+              <Route
+                path="about"
+                element={<About agent={agent[0]} Id={agentId} />}
+              />
+              <Route
+                path="calendar"
+                element={<Calendar agent={agent[0]} Id={agentId} />}
+              />
+              <Route
+                path="calculator"
+                element={<Calculator agent={agent[0]} Id={agentId} />}
+              />
+              <Route
+                path="faq"
+                element={<FAQ agent={agent[0]} Id={agentId} />}
+              />
+              <Route
+                path="videoseries"
+                element={<VideoSeries agent={agent[0]} Id={agentId} />}
+              />
+            </Routes>
+          </BrowserRouter>
+        </Col>
+        {/* <Col xs={2} style={{ color: `white`, position:"relative", padding: "0px" }}>
+          <SideMenu agent={agent[0]} Id={agentId} />
+        </Col> */}
+      </Row>
+      <Footer agent={agent[0]} Id={agentId} style={{ marginLeft: "0px" }}/>
+      {/* <Bottom /> */}
+    </div>
+  );
+}
