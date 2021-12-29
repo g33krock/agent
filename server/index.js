2bc5a63db8a9ec5dd5278d39eba2f802832dacf9
Dallas Lovell <dallas.lovell@gmail.com>
2021-12-28 22:31:47 -0700

    more work on client/server relations 2

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const cors = require("cors");
 const db = require("./db");
-const port = process.env.DB_PORT || 5000;
+const port = process.env.PORT || 5000;
 const staticPath = path.join(__dirname, '../client/build')
 
 app.use(bodyParser.json());
