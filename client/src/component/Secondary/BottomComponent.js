45e89e15d6802a28e36fe9456e6c5c92a00d9e2a
Dallas Lovell <dallas.lovell@gmail.com>
2022-01-13 14:12:50 -0700

    New Footer 4

--- client/src/component/Secondary/BottomComponent.js
+++ client/src/component/Secondary/BottomComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Container, Row, Col, Button, Image } from "react-bootstrap";
 import { agentService } from "../../services/AgentService";
+import { BottomCalendar } from "./BottomCalendarComponent";
 
 export class Bottom extends Component {
   constructor(props) {
@@ -65,38 +66,38 @@ export class Bottom extends Component {
             </Col>
             <Col />
             <Col sm={3}>
-              <Button
-                style={{
-                  // backgroundColor: `${this.props.agent.secondaryColor}`,
-                  backgroundColor: `black`,
-                  color: `white`,
-                  width: "100%",
-                  border: "none",
-                  boxShadow: "2px 2px rgba(0, 0, 0, 0.5)",
-                  marginBottom: "5px",
-                }}
-                href="/videoseries"
-              >
-                SCHEDULE A 1 ON 1 WITH A
-                <br />
-                MPI® CERTIFIED ADVISOR
-              </Button>{" "}
+              {this.state.agent &&
+                this.state.agent.map((ag) => <BottomCalendar agent={ag}/>)}
             </Col>
             <Col />
           </Row>
           {this.state.agent &&
             this.state.agent.map((ag) => (
               <>
-                <Row style={{paddingBottom: "5%"}}><Image src={ag.icon} style={{maxHeight: "100px", width: "auto", margin: "auto"}} /></Row>
+                <Row style={{ paddingBottom: "5%" }}>
+                  <Image
+                    src={ag.icon}
+                    style={{
+                      maxHeight: "100px",
+                      width: "auto",
+                      margin: "auto",
+                    }}
+                  />
+                </Row>
                 <Row
                   style={{
                     minHeight: "10vh",
-                    backgroundColor: ag.primaryColor
+                    backgroundColor: ag.primaryColor,
                   }}
                 >
                   <Col>
                     <p
-                      style={{ color: "white", textShadow: "1px 1px black", marginTop: "1%", marginBottom: "1%" }}
+                      style={{
+                        color: "white",
+                        textShadow: "1px 1px black",
+                        marginTop: "1%",
+                        marginBottom: "1%",
+                      }}
                     >
                       {ag.address1}
                       <br />
--- client/src/component/Secondary/FooterComponent.js
+++ client/src/component/Secondary/FooterComponent.js
@@ -33,80 +33,83 @@ export class Footer extends Component {
             >
               <Row style={{ marginLeft: "auto", marginRight: "auto" }}>
                 <Col xs={2} sm={3} md={4} />
-                {ag.facebook && (
-                  <Col style={{ padding: "1%" }}>
-                    <a href={ag.facebook}>
-                      <Image
-                        fluid
-                        src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Facebook.ico"
-                      />
-                    </a>
-                  </Col>
-                )}
-                {ag.twitter && (
-                  <Col style={{ padding: "1%" }}>
-                    <a href={ag.twitter}>
-                      <Image
-                        fluid
-                        src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Twitter.ico"
-                      />
-                    </a>
-                  </Col>
-                )}
-                {ag.instagram && (
-                  <Col style={{ padding: "1%" }}>
-                    <a href={ag.instagram}>
-                      <Image
-                        fluid
-                        src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Instagram.ico"
-                      />
-                    </a>
-                  </Col>
-                )}
-                {ag.tiktok && (
-                  <Col style={{ padding: "1%" }}>
-                    <a href={ag.tiktok}>
-                      <Image
-                        fluid
-                        src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/TikTok.ico"
-                      />
-                    </a>
-                  </Col>
-                )}
-                {ag.youtube && (
-                  <Col style={{ padding: "1%" }}>
-                    <a href={ag.youtube}>
-                      <Image
-                        fluid
-                        src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/YouTube.ico"
-                      />
-                    </a>
-                  </Col>
-                )}
-                {ag.pinterest && (
-                  <Col style={{ padding: "1%" }}>
-                    <a href={ag.pinterest}>
-                      <Image
-                        fluid
-                        src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Pinterest.ico"
-                      />
-                    </a>
-                  </Col>
-                )}
+                <Col style={{marginLeft: '9%', marginRight: '9%'}}>
+                <Row>
+                  {ag.facebook && (
+                    <Col style={{ padding: "1%" }}>
+                      <a href={ag.facebook}>
+                        <Image
+                          fluid
+                          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Facebook.ico"
+                        />
+                      </a>
+                    </Col>
+                  )}
+                  {ag.twitter && (
+                    <Col style={{ padding: "1%" }}>
+                      <a href={ag.twitter}>
+                        <Image
+                          fluid
+                          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Twitter.ico"
+                        />
+                      </a>
+                    </Col>
+                  )}
+                  {ag.instagram && (
+                    <Col style={{ padding: "1%" }}>
+                      <a href={ag.instagram}>
+                        <Image
+                          fluid
+                          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Instagram.ico"
+                        />
+                      </a>
+                    </Col>
+                  )}
+                  {ag.tiktok && (
+                    <Col style={{ padding: "1%" }}>
+                      <a href={ag.tiktok}>
+                        <Image
+                          fluid
+                          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/TikTok.ico"
+                        />
+                      </a>
+                    </Col>
+                  )}
+                  {ag.youtube && (
+                    <Col style={{ padding: "1%" }}>
+                      <a href={ag.youtube}>
+                        <Image
+                          fluid
+                          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/YouTube.ico"
+                        />
+                      </a>
+                    </Col>
+                  )}
+                  {ag.pinterest && (
+                    <Col style={{ padding: "1%" }}>
+                      <a href={ag.pinterest}>
+                        <Image
+                          fluid
+                          src="https://jwvonytjrpiueyuwsjpa.supabase.in/storage/v1/object/public/icons/Pinterest.ico"
+                        />
+                      </a>
+                    </Col>
+                  )}
+                </Row>
+                </Col>
                 <Col xs={2} sm={3} md={4} />
               </Row>
               <Row>
-
-                  <p style={{color: 'white', fontSize: '80%'}}>
-                    Copyright © 2022 MPI® Unlimited LLC. All Rights Reserved.<br />
-                    This site is not part of the Facebook website or Facebook
-                    Inc. Additionally, this site is NOT endorsed by Facebook,
-                    TikTok or Pinterest in any way.<br /> Facebook is a trademark of
-                    FACEBOOK, Inc. This site is not part of the TikTok website
-                    or TikTok Company. This site is not part of the Pinterest
-                    website or Pinterest Company.
-                  </p>
-
+                <p style={{ color: "white", fontSize: "80%" }}>
+                  Copyright © 2022 MPI® Unlimited LLC. All Rights Reserved.
+                  <br />
+                  This site is not part of the Facebook website or Facebook Inc.
+                  Additionally, this site is NOT endorsed by Facebook, TikTok or
+                  Pinterest in any way.
+                  <br /> Facebook is a trademark of FACEBOOK, Inc. This site is
+                  not part of the TikTok website or TikTok Company. This site is
+                  not part of the Pinterest website or Pinterest Company.
+                </p>
               </Row>
             </Row>
           ))}
