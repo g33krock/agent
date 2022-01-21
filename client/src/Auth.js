30abf3431af14830fb45fa8b6790917ad1c9e834
Dallas Lovell <dallas.lovell@gmail.com>
2022-01-21 11:16:57 -0700

    Magic Link

--- client/src/Auth.js
+++ client/src/Auth.js
@@ -1,3 +1,54 @@
+// import { supabase } from "./supabaseClient";
+// import React, { useContext, useState, useEffect } from 'react'
+
+
+// const AuthContext = React.createContext()
+
+// export default function Auth({ children }) {
+//     const [user, setUser] = useState()
+//     const [token, setToken] = useState()
+//     const [loading, setLoading] = useState(true)
+  
+//     useEffect(() => {
+//       // Check active sessions and sets the user
+//       const session = supabase.auth.session()
+  
+//       setUser(session?.user ?? null)
+//       setLoading(false)
+  
+//       // Listen for changes on auth state (logged in, signed out, etc.)
+//       const { data: listener } = supabase.auth.onAuthStateChange(
+//         async (event, session) => {
+//           setToken(session?.access_token ?? null);
+//           setUser(session?.user ?? null)
+//           setLoading(false)
+//         }
+//       )
+  
+//       return () => {
+//         listener?.unsubscribe()
+//       }
+//     }, [])
+  
+//     // Will be passed down to Signup, Login and Dashboard components
+//     const value = {
+//       signUp: (data) => supabase.auth.signUp(data),
+//       signIn: (data) => supabase.auth.signIn(data),
+//       signOut: () => supabase.auth.signOut(),
+//       user,
+//     }
+  
+//     return (
+//       <AuthContext.Provider value={value}>
+//         {!loading && children}
+//       </AuthContext.Provider>
+//     )
+//   }
+
+//   export function useAuth() {
+//     return useContext(AuthContext)
+//   }
+
 import {
     Box,
     Button,
@@ -17,7 +68,7 @@ import { supabase } from "./supabaseClient";
 export default function Auth() {
     const [loading, setLoading] = useState(false);
     const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    // const [password, setPassword] = useState('');
     const toast = useToast();
 
     const handleLogin = async (email, password) => {
@@ -62,10 +113,6 @@ export default function Auth() {
                 <FormControl id="email">
                   <FormLabel>Email address</FormLabel>
                   <Input value={email} onChange={e => setEmail(e.target.value)} type="email" />
-                </FormControl>
-                <FormControl id="password">
-                  <FormLabel>Password</FormLabel>
-                  <Input value={password} onChange={e => setPassword(e.target.value)} type="password" />
                 </FormControl>
                 <Stack spacing={10}>
                   <Button
