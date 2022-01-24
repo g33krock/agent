01187998c863d9f65de8566ff3936e5af77e34af
g33krock <dallas.lovell@gmail.com>
2022-01-23 19:26:40 -0700

    New Login Method with Email, Password, and Email Recovery

--- client/src/component/MainComponent.js
+++ client/src/component/MainComponent.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Client from 'shopify-buy';
+import Client from "shopify-buy";
 import { agentService } from "../services/AgentService";
 import { About } from "./About/AboutComponent";
 import { AboutCurtis } from "./About/AboutCurtisComponent";
@@ -16,6 +16,11 @@ import { Home } from "./HomePage/HomeComponent";
 import { VideoSeries } from "./VideoSeries/VideoSeriesComponent";
 import Shopify from "./Shopify/ShopifyComponent";
 import SignIn from "./SignInComponent";
+import { Dashboard } from "./Dashboard";
+import { Login } from "./Login";
+import { Signup } from "./Signup";
+import { AuthProvider } from "../contexts/Auth";
+import RecoverPassword from "./RecoverComponent";
 
 export function Main(props) {
   const agentId = 12;
@@ -24,18 +29,23 @@ export function Main(props) {
     const initialAgent = agentService.one(agentObject);
     return initialAgent;
   });
+  const [recoveryToken, setRecoveryToken] = useState(null);
   const paidVideo = props.vids;
   const paidDeposit = props.deposit;
   console.log(paidVideo);
   console.log(paidDeposit);
 
   const client = Client.buildClient({
-    domain: 'mpiunlimited.myshopify.com',
-    storefrontAccessToken: '9ce898b59cd04f20cd3e147fbfa95af2',
+    domain: "mpiunlimited.myshopify.com",
+    storefrontAccessToken: "9ce898b59cd04f20cd3e147fbfa95af2",
   });
-  return (
+  return recoveryToken ? (
+    <RecoverPassword
+      token={recoveryToken}
+      setRecoveryToken={setRecoveryToken}
+    />
+  ) : (
     <div style={{ minHeight: "100vh" }}>
-
       <Header2 agent={agent[0]} Id={agentId} style={{ marginLeft: "0px" }} />
       <Calendar agent={agent[0]} Id={agentId}></Calendar>
       <Row
@@ -44,57 +54,96 @@ export function Main(props) {
           marginLeft: "0px",
           width: "100%",
           minHeight: "80vh",
-          marginTop: "49px"
+          marginTop: "49px",
         }}
       >
         <Col style={{ paddingRight: "0px", paddingLeft: "0px" }}>
           <BrowserRouter>
-            <Routes>
-              <Route
-                path="/"
-                element={<Home agent={agent[0]} Id={agentId} vids={paidVideo} deposit={paidDeposit} />}
-              />
-              <Route
-                path="about"
-                element={<About agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="calendar"
-                element={<Calendar agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="calculator"
-                element={<Calculator agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="faq"
-                element={<FAQ agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="videoseries"
-                element={<VideoSeries agent={agent[0]} Id={agentId} vids={paidVideo} deposit={paidDeposit} />}
-              />
-              <Route
-                path="contactus"
-                element={<ContactUs agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="curtisray"
-                element={<AboutCurtis agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="webinarregistration"
-                element={<Webinar agent={agent[0]} Id={agentId} />}
-              />
-              <Route
-                path="shopify"
-                element={<Shopify agent={agent[0]} Id={agentId} client={client}/>}
-              />
-              <Route
-                path="signin"
-                element={<SignIn agent={agent[0]} Id={agentId} client={client}/>}
-              />
-            </Routes>
+            <AuthProvider>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <Home
+                      agent={agent[0]}
+                      Id={agentId}
+                      vids={paidVideo}
+                      deposit={paidDeposit}
+                    />
+                  }
+                />
+                <Route
+                  path="about"
+                  element={<About agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="calendar"
+                  element={<Calendar agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="calculator"
+                  element={<Calculator agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="faq"
+                  element={<FAQ agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="videoseries"
+                  element={
+                    <VideoSeries
+                      agent={agent[0]}
+                      Id={agentId}
+                      vids={paidVideo}
+                      deposit={paidDeposit}
+                    />
+                  }
+                />
+                <Route
+                  path="contactus"
+                  element={<ContactUs agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="curtisray"
+                  element={<AboutCurtis agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="webinarregistration"
+                  element={<Webinar agent={agent[0]} Id={agentId} />}
+                />
+                <Route
+                  path="shopify"
+                  element={
+                    <Shopify agent={agent[0]} Id={agentId} client={client} />
+                  }
+                />
+                <Route
+                  path="signin"
+                  element={
+                    <SignIn agent={agent[0]} Id={agentId} client={client} />
+                  }
+                />
+
+                <Route
+                  path="dashboard"
+                  element={
+                    <Dashboard agent={agent[0]} Id={agentId} client={client} />
+                  }
+                />
+                <Route
+                  path="login"
+                  element={
+                    <Login agent={agent[0]} Id={agentId} client={client} />
+                  }
+                />
+                <Route
+                  path="signup"
+                  element={
+                    <Signup agent={agent[0]} Id={agentId} client={client} />
+                  }
+                />
+              </Routes>
+            </AuthProvider>
           </BrowserRouter>
         </Col>
       </Row>
