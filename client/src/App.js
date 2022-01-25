e27424f099e888c50276fc26182043facfdfb37f
Dallas Lovell <dallas.lovell@gmail.com>
2022-01-25 13:19:00 -0700

    Another Attempt at Password Recovery 4

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { useAuth } from './Auth';
 import { Main } from './component/MainComponent';
 import { useState } from 'react';
+import RecoverPassword from "./RecoverComponent";
 
 function App() {
   const user = useAuth();
   const [paidVideo] = useState(true);
   const [paidDeposit] = useState(true)
+  const params = new URLSearchParams(window.location.hash);
+
+  const accessToken = params.get("access_token");
+  console.log(accessToken);
+  const [recoveryToken, setRecoveryToken] = useState(accessToken);
 
   console.log(user)
-  return (
+  return recoveryToken ? (
+    <RecoverPassword
+      token={recoveryToken}
+      setRecoveryToken={setRecoveryToken}
+      user={user}
+    />
+  ) : (
     <div className="App" >
       <Main vids={paidVideo} deposit={paidDeposit} userEmail={user?.email}/>
     </div>
