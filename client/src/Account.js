c36b79eb68f3a9320bff2d8e76eb9e71f2b7458d
Dallas Lovell <dallas.lovell@gmail.com>
2022-01-28 14:54:38 -0700

    Defaults paidVideo & paidDeposit 4

--- client/src/Account.js
+++ client/src/Account.js
@@ -150,7 +150,7 @@ export default function Account({ session }) {
               <FormLabel>Paid Video</FormLabel>
               <Switch value={1}
                 onChange={e => setPaidVideo(e.target.value)}
-                defaultIsChecked={"paidVideo" === true ? true : false}
+                defaultIsChecked={"paidVideo" === false ? false : true}
                 color={useColorModeValue("gray.800", "gray.200")}
                 bg={useColorModeValue("gray.100", "gray.600")}
                 rounded={"full"}
